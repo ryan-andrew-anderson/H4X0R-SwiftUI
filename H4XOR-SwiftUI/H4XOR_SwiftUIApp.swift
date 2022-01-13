0bb0aa4f57a881132fb1c16b77f9de7d68b6230c
Ryan A. Anderson <ryan.anderson9921@stu.mtec.edu>
2022-01-13 07:41:01 -0700

    Initial Commit

---
+++ H4XOR-SwiftUI/H4XOR_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_SwiftUIApp.swift
+//  H4XOR-SwiftUI
+//
+//  Created by Ryan Anderson on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
